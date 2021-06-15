3900da81dadb1f49c2ff8e5f0b994e171781303b
itzfidel <7500202271@unjfsc.edu.pe>
2021-06-15 16:15:47 -0500

    29JUN2021

--- UsuarioController.php
+++ UsuarioController.php
@@ -2,7 +2,7 @@
 include_once '../modelo/Usuario.php';
 $usuario = new Usuario();
 session_start();
-$id_usuario = $_SESSION['usuario'];
+$id_usuario= $_SESSION['usuario'];
 if($_POST['funcion']=='buscar_usuario'){
     $json=array();
     $fecha_actual = new DateTime();
@@ -22,7 +22,8 @@ if($_POST['funcion']=='buscar_usuario'){
             'residencia'=>$objeto->residencia_us,
             'correo'=>$objeto->correo_us,
             'sexo'=>$objeto->sexo_us,
-            'adicional'=>$objeto->adicional_us
+            'adicional'=>$objeto->adicional_us,
+            'avatar'=>'../img/'.$objeto->avatar
 
         );
     }
@@ -63,6 +64,71 @@ if($_POST['funcion']=='cambiar_contra'){
     $oldpass=$_POST['oldpass'];
     $newpass=$_POST['newpass'];
     $usuario->cambiar_contra($id_usuario,$oldpass,$newpass);
+}
+if($_POST['funcion']=='cambiar_foto'){ 
+    if(($_FILES['photo']['type']=='image/jpeg')||($_FILES['photo']['type']=='image/png')||($_FILES['photo']['type']=='image/gif')){
+        $nombre=uniqid().'-'.$_FILES['photo']['name'];
+        $ruta='../img/'.$nombre;
+        move_uploaded_file($_FILES['photo']['tmp_name'],$ruta);
+        $usuario->cambiar_photo($id_usuario,$nombre);
+        foreach($usuario->objetos as $objeto) {
+            unlink('../img/'.$objeto->avatar);
+        }
+        $json= array();
+        $json[]=array(
+            'ruta'=>$ruta,
+            'alert'=>'edit'
+        );
+        $jsonstring = json_encode($json[0]);
+        echo $jsonstring;
+    } 
+    else{
+        $json= array();
+        $json[]=array(
+            'alert'=>'noedit'
+        );
+        $jsonstring = json_encode($json[0]);
+        echo $jsonstring;
+
+    }
+}
+if($_POST['funcion']=='buscar_usuarios_adm'){
+    $json=array();
+    $fecha_actual = new DateTime();
+    $usuario->buscar();
+    foreach ($usuario->objetos as $objeto) {
+        $nacimiento = new DateTime($objeto->edad);
+        $edad = $nacimiento->diff($fecha_actual);
+        $edad_years = $edad->y;
+        $json[]=array(
+            'nombre'=>$objeto->nombre_us,
+            'apellidos'=>$objeto->apellidos_us,
+            'edad'=>$edad_years,
+            'dni'=>$objeto->dni_us,
+            'tipo'=>$objeto->nombre_tipo,
+            'telefono'=>$objeto->telefono_us,
+            'movil'=>$objeto->movil_us,
+            'residencia'=>$objeto->residencia_us,
+            'correo'=>$objeto->correo_us,
+            'sexo'=>$objeto->sexo_us,
+            'adicional'=>$objeto->adicional_us,
+            'avatar'=>'../img/'.$objeto->avatar,
+            'tipo_usuario'=>$objeto->us_tipo
+
+        );
+    }
+    $jsonstring = json_encode($json);
+    echo $jsonstring;
+}
+if($_POST['funcion']=='crear_usuario'){
+    $nombre = $_POST['nombre'];
+    $apellido = $_POST['apellido'];
+    $edad = $_POST['edad'];
+    $dni = $_POST['dni'];
+    $pass = $_POST['pass'];
+    $tipo=2;
+    $avatar='';
+    $usuario->crear($nombre,$apellido,$edad,$dni,$pass,$tipo,$avatar);
 }
 ?>
 
